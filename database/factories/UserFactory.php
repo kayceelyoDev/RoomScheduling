6ae0e375e1d1f213c2d907e1b54321ed5909e494
Kent clarence Evangelista <kentclarence5368@gmail.com>
2026-04-17 01:22:23 +0800

    fix again

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,13 +24,17 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // 1. Manually create the Faker instance
+        $faker = \Faker\Factory::create();
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            // 2. Use the local $faker variable
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'role' => $this->faker->randomElement(['admin', 'user']),
+            'role' => $faker->randomElement(['admin', 'user']),
         ];
     }
 
