28e725c19d03c6b773fa1966efcae5d922cf4796
Kayceelyo_dev <kentclarence5368@gmail.com>
2026-04-21 13:35:10 +0800

    Change option values to lowercase in status select

--- resources/views/classrooms/createClassroom.blade.php
+++ resources/views/classrooms/createClassroom.blade.php
@@ -89,9 +89,9 @@
                                     <select name="status" id="status" required
                                             class="block w-full pl-10 py-2.5 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                         <option value="" disabled selected>Select Status</option>
-                                        <option value="Available">Available</option>
-                                        <option value="Maintenance">Maintenance</option>
-                                        <option value="Occupied">Occupied</option>
+                                        <option value="available">Available</option>
+                                        <option value="maintenance">Maintenance</option>
+                                        <option value="occupied">Occupied</option>
                                     </select>
                                 </div>
                                 @error('status')
